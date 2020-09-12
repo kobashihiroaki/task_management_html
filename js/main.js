bf3ec5c3cfd2cd61d84b825f2e8962df9481a2bb
hiroaki <koushou418@gmail.com>
2020-09-13 00:12:50 +0900

    外側をクリックしたときに閉じる

--- js/main.js
+++ js/main.js
@@ -1,56 +1,92 @@
 let task_input = document.getElementById("task-input");
 let input_box = document.getElementById("input-box");
+let task_edit = document.getElementById("task-edit");
+let gray = document.getElementById("gray");
+let flag = "close";
+let form_flag = "";
 
+function close_flag() {
+    flag = "close";
+}
+
+function open_flag() {
+    flag = "open";
+}
 //タスク入力画面を押したとき
 function open_form() {
     task_input.style.display = 'none';
     input_box.style.display = 'block';
+    // open_flag();
+    form_flag = "input";
 }
 
 document.addEventListener('click', (e) => {
-    if(!e.target.closest('.input-box')) {
-      //外側をクリックしたときの処理
-      task_input.style.display = 'block';
-      input_box.style.display = 'none';
-    } else {
-      //内側をクリックしたときの処理
-      task_input.style.display = 'none';
-      input_box.style.display = 'block';
-    }
+        console.log(flag);
+        console.log(form_flag);
+        if (flag === "close") {
+              if (form_flag !== "") {
+                open_flag();
+              }
+              return;
+        }
+        if (form_flag === "input") {
+              if(!e.target.closest('.input-box')) {
+                //入力フォームの外側をクリックしたときの処理
+                //タスク編集フォームの外側をクリック
+                if(!e.target.closest('.task')) {
+                  task_input.style.display = 'flex';
+                  input_box.style.display = 'none';
+                  close_flag();
+                  form_flag = "";
+                //タスク編集フォームの内側をクリック
+                } else {
+                  task_input.style.display = 'flex';
+                  input_box.style.display = 'none';
+                  gray.style.display = 'flex';
+                  form_flag = "edit";
+                }
+              } else {
+                //内側をクリックしたときの処理
+                //閉じるボタン以外をクリック
+                if (!e.target.closest('.close-form')) {
+                  task_input.style.display = 'none';
+                  input_box.style.display = 'block';
+                //閉じるボタンをクリック
+                } else {
+                  task_input.style.display = 'flex';
+                  input_box.style.display = 'none';
+                  close_flag();
+                  form_flag = "";
+                }
+              }
+        } else if (form_flag === "edit") {
+          if (flag === "close") {
+                open_flag();
+                return;
+          }
+          if(!e.target.closest('.task-edit')) {
+            //外側をクリックしたときの処理
+              gray.style.display = 'none';
+              close_flag();
+              form_flag = "";
+          } else {
+            //内側をクリックしたときの処理
+              if(!e.target.closest('#close-task')) {
+                  gray.style.display = 'flex';
+              } else {
+                  gray.style.display = 'none';
+                  close_flag();
+                  form_flag = "";
+              }
+          }
+        }
 });
 
-//閉じるボタンを押したとき
-document.getElementById("close").addEventListener('click', function() {
-    task_input.style.display = 'block';
-    input_box.style.display = 'none';
-});
-
-let task_edit = document.getElementById("task-edit");
-
-//タスクを押したとき
+// タスクを押したとき
 function edit_task() {
-    task_edit.style.display = 'block';
+    if(form_flag === "input") {
+      return;
+    }
+    gray.style.display = 'flex';
+    form_flag = "edit";
 }
-
-//タスクの閉じるボタンを押したとき
-document.getElementById("close-task").addEventListener('click', function() {
-    task_edit.style.display = 'none';
-});
-
-function close_form() {
-    task_edit.style.display = 'none';
-}
-
-// //何もないところをクリックしたとき
-// document.addEventListener('click', (e) => {
-//     if(!e.target.closest('.input-box')) {
-//       //外側をクリックしたときの処理
-//         task_input.style.display = 'block';
-//         input_box.style.display = 'none';
-//     } else {
-//       //内側をクリックしたときの処理
-//         task_input.style.display = 'none';
-//         input_box.style.display = 'block';
-//     }
-// });
-
